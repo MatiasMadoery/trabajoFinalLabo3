e1a16475f9e19affe1a37a0d86506358c32c8733
MatiasMAdoery <matimadoery@hotmail.com>
2024-05-14 11:32:18 -0300

    Se agrega el proyecto

---
+++ trabajoFinalLabo3/script.js
@@ -0,0 +1,57 @@
+function DesplegarMenuHam() 
+{
+    var desplegarMenu = document.getElementById('navHambur')
+    var body = document.body;
+
+    if(desplegarMenu)
+    {
+        if(desplegarMenu.className == 'navHamburCerrado')
+        {
+            desplegarMenu.className = 'navHambur';
+            body.classList.add('menuAbierto');
+        }
+        else
+        {
+            desplegarMenu.className = 'navHamburCerrado';
+            body.classList.remove('menuAbierto');
+        }
+    } 
+}
+
+function DesplegarMenuProductos() 
+{
+    var desplegarMenu = document.getElementById('navProductos')
+    var body = document.body;
+
+    if(desplegarMenu)
+    {
+        if(desplegarMenu.className == 'navProductosCerrado')
+        {
+            desplegarMenu.className = 'navProductos';
+            body.classList.add('menuAbierto');
+        }
+        else
+        {
+            desplegarMenu.className = 'navProductosCerrado';
+            body.classList.remove('menuAbierto');
+        }
+    } 
+}
+
+function Buscar() {
+    let entrada = document.getElementById("parametro");
+    let terminoBuscado = entrada.value.toLowerCase();
+    
+    let secciones = document.querySelectorAll("#contenedor section");
+    
+    secciones.forEach(seccion => {
+        let titulo = seccion.querySelector("h1");
+        if (titulo) {
+            if (titulo.textContent.toLowerCase().includes(terminoBuscado)) {
+                seccion.removeAttribute('style');
+            } else {
+                seccion.setAttribute('style', 'display: none');
+            }
+        }
+    });
+}
