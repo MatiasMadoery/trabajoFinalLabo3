a848b071c6f4163411717b352ce8f3e176b144c1
MatiasMAdoery <matimadoery@hotmail.com>
2024-06-20 20:15:54 -0300

    se agregan mediaQueries al css

--- trabajoFinalLabo3/js/footer.js
+++ trabajoFinalLabo3/js/footer.js
@@ -1,40 +1,24 @@
 document.write(`
        <footer>
         <div class="contacto-redes">
-            <section id="contacto">
+            <div id="contacto">
                 <h4>Contacto</h4>
+                <p>Teléfono: +54 03564 15507216</p>
+                <p>Email: amoeldisenoventas@gmail.com</p>                
+                <p>Direccion: Peru 28 - San Francisco - Cordoba - Argentina</p>
+            </div>
+            <div id="redes">
+                <h4>Redes Sociales</h4>
                 <ul>
-                    <li>
-                        Direccion: Peru 28 - San Francisco - Cordoba - Argentina
-                    </li>
-                    <li>
-                        Telefono: +54 03564 15507216
-                    </li>
-                    <li>
-                        Email: amoeldisenoventas@gamil.com
-                    </li>
+                    <li><img src="img/facebook.png" alt="Facebook" id="iconoRedes">Facebook</li>                    
+                    <li><img src="img/instagram.png" alt="Instagram" id="iconoRedes">Instagram</li>
                 </ul>
-            </section>
-            <section id="redes">
-                <h4>Redes</h4>
-                <ul>
-                    <li>
-                        <img id="iconoRedes" src="/trabajoFinalLabo3/img/facebook.png" alt="Icono Facebook">
-                    </li>
-                    <li>
-                        <img id="iconoRedes" src="/trabajoFinalLabo3/img/instagram.png" alt="Icono Instragram">
-                    </li>             
-                </ul>
-            </section>
-        </div>        
+            </div>
+        </div>
         <div id="logoFooter">
-            <img src="imagen1.jpg" alt="Imagen 1">
-        </div>     
-        <p id="pFooter">
-            <i>Copyright 2024 &copy; AMOELDISEÑO SA.</i>             
-        </p>
-        <p id="pFooter2">
-            Desarrollado por <i>Matias Madoery</i>.
-        </p>           
+            <img src="img/logo.png" alt="Logo">
+        </div>
+        <p id="pFooter">© 2023 Amo el diseño. Todos los derechos reservados.</p>
+        <p id="pFooter2">Diseñado por Matias Madoery.</p>
     </footer>
     `)
